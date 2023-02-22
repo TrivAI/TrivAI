cda62c1b57c384e54f4396587207ad136b9646e7
Victor Mercado <victormercado@outlook.com>
2023-02-21 18:04:47 -0800

    fixing type error

--- app/components/ClientQuiz.tsx
+++ app/components/ClientQuiz.tsx
@@ -4,14 +4,16 @@ import { useState, useEffect } from 'react';
 
 
 export default function ClientQuiz() {
-    const [pokemon, setPokemon] = useState(null);
+    const [pokemon, setPokemon] = useState<any>({});
     const [brightness, setBrightness] = useState(0);
     useEffect(() => {
         const id = Math.floor(Math.random() * 898) + 1;
         fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
             .then(res => res.json())
             .then(data => {
-                setPokemon(data as any);
+                let name = data!.name;
+                let sprite = data!.sprites.other['official-artwork']['front_default'];
+                setPokemon({pokemonName: name, sprite: sprite});
             });
     },[]);
     const {count, name, inc} = useStore((state) => state);
@@ -27,9 +29,9 @@ export default function ClientQuiz() {
             
             {pokemon && (  
                 <div className='m-4 flex flex-col'>
-                    <h1>{pokemon!.name}</h1>
+                    <h1>{pokemon.pokemonName}</h1>
                     <div className='bg-white'>
-                        <img className= {`brightness-${brightness}`} src={pokemon!.sprites.other['official-artwork']['front_default']} />
+                        <img className= {`brightness-${brightness}`} src={pokemon.sprites} />
                     </div>
                     <button className="mt-4 p-2 border border-blue-500" onClick={() => setBrightness(100)}>reveal pokemon</button>
                 </div>
