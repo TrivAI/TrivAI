2b98ea792e941dbf9f173a89eee844e9030ba2bb
Victor Mercado <victormercado@outlook.com>
2023-05-07 23:17:14 -0700

    small fix to update URL

--- src/checkEnvironment.js
+++ src/checkEnvironment.js
@@ -6,7 +6,7 @@ export default function checkEnvironment(whereisitran) {
   let base_url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "http://localhost:3000";
+      : "https://triv-ai.vercel.app/";
       
       // "https://triv-ai.vercel.app/"; // https://v2ds.netlify.app
 
